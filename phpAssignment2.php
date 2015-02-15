02bb66f868c045abbbcbf992c6ac73a7882f34c3
Adam Martinez <adam70181@yahoo.com>
2015-02-15 14:05:17 -0600

    html boxes with error messages

--- phpAssignment2.php
+++ phpAssignment2.php
@@ -1,3 +1,23 @@
+<html>
+<head>
+	<title>mySql</title>
+</head>
+<body>
+	<form action="phpAssignment2.php" method="get">
+	<label for="name">Name</label>
+	<input type="text" name="name" id="name" /><br/>
+	
+	<label for="category">Category</label>
+	<input type="text" name="category" id="category" /><br/>
+	
+	<label for="length">Length</label>
+	<input type="text" name="length" id="length" /><br/>
+
+	<input type="submit" value="Add"/>
+
+</body>
+</html>
+
 <?php
 
 include 'storedInfo.php';
@@ -5,6 +25,28 @@ $db = new mysqli('oniddb.cws.oregonstate.edu', 'martinad-db', $myPassword, 'mart
 if($db->connect_errno) {
 	die('No database connection');
 } else {
-	echo 'Database found';
+	echo 'Database found', '<br>';
 }
+
+if(isset($_GET['name'])) {
+	if(trim($_GET['name']) == "") {
+	echo 'You must enter a video name.', '<br>';
+}
+}
+
+if(isset($_GET['category'])) {
+	if(trim($_GET['category']) == "") {
+	echo 'You must enter a category.', '<br>';
+} 
+}
+
+if(isset($_GET['length'])) {
+	$len = trim($_GET['length']);
+	if(empty($len)) {
+		echo 'You must enter a length.';
+	} else if (!(ctype_digit($len))) {
+		echo 'Length must be a numeric value';
+	}
+}
+	
 ?>
