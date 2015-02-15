faf11fdbb1a02a72f07ef68bbb08c58d5acbd3e6
Adam Martinez <adam70181@yahoo.com>
2015-02-15 17:53:43 -0600

    delete is working

--- phpAssignment2.php
+++ phpAssignment2.php
@@ -13,7 +13,8 @@
 	<label for="length">Length</label>
 	<input type="text" name="length" id="length" /><br/>
 
-	<input type="submit" value="Add"/>
+	<input type="submit" value="Add"/><br/>
+	</form>
 
 </body>
 </html>
@@ -54,6 +55,21 @@ if(isset($_GET['length'])) {
 	}
 }
 
+//delete
+if(isset($_POST['delete'])) {
+	echo 'delete';
+	if (!($stmt = $db->prepare("DELETE FROM videoStore WHERE id = ?"))) {
+		echo "Prepare failed: (" . $mysqli->errno . ") " . $mysqli->error;
+	}
+
+	if (!$stmt->bind_param("i", $_POST['vid'])) {
+		echo "Binding parameters failed: (" . $stmt->errno . ") " . $stmt->error;
+	}
+
+	if (!$stmt->execute()) {
+		echo "Execute failed: (" . $stmt->errno . ") " . $stmt->error;
+	}
+}
 
 
 
@@ -74,17 +90,30 @@ if (!$stmt->execute()) {
 }
 
 //display table
-if (!($result = $db->prepare("SELECT name, category, length FROM videoStore"))) {
+if (!($result = $db->prepare("SELECT id, name, category, length FROM videoStore"))) {
     echo "Prepare failed: (" . $mysqli->errno . ") " . $mysqli->error;
 }
 
 $result->execute();
-$result->bind_result($videoName, $videoCat, $len);
+$result->bind_result($id, $videoName, $videoCat, $len);
 
+echo "<table border=1>
+<tr>
+<th>ID</th>
+<th>Name</th>
+<th>Category</th>
+<th>Length</th>
+</tr>";
 while($result->fetch()) {
-		echo $videoName, ' ';
-		echo $videoCat, ' ';
-		echo $len, '<br>';
+	    echo "<form action=phpAssignment2.php method=post>";
+	    echo "<tr>";
+		echo "<td>" . "<input type=text name=vid value=" . $id . " </td>";
+		echo "<td>" . "<input type=text name=vName value=" . $videoName . " </td>";
+		echo "<td>" . "<input type=text name=vCat value=" . $videoCat . " </td>";
+		echo "<td>" . "<input type=text name=vLen value=" . $len . " </td>";
+		echo "<td>" . "<input type=submit name=delete value='delete'" . "</td>";
+		echo "</tr>";
+		echo "</form>";
 	}
-	
+	echo "</table>";
 ?>
