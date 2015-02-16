68a88ec863f763c4669d39fb00a00e549146c096
Adam Martinez <adam70181@yahoo.com>
2015-02-15 22:37:31 -0600

    final, turn in

--- phpAssignment2.php
+++ phpAssignment2.php
@@ -23,6 +23,7 @@
 
 include 'storedInfo.php';
 $notValid = 0;
+$up = 0;
 $db = new mysqli('oniddb.cws.oregonstate.edu', 'martinad-db', $myPassword, 'martinad-db');
 if ($db->connect_errno) {
     echo "Failed to connect to MySQL: (" . $db->connect_errno . ") " . $db->connect_error;
@@ -38,11 +39,11 @@ if(isset($_GET['name'])) {
 }
 
 if(isset($_GET['category'])) {
-	$videoCat = $_GET['category'];
-	if(trim($videoCat) == "") {
-	echo 'You must enter a category.', '<br>';
-	$notValid = 1;
-} 
+		$videoCat = $_GET['category'];
+		if(trim($videoCat) == "") {
+		echo 'You must enter a category.', '<br>';
+		$notValid = 1;
+	} 
 }
 
 if(isset($_GET['length'])) {
@@ -55,20 +56,38 @@ if(isset($_GET['length'])) {
 	}
 }
 
-//delete
-if(isset($_POST['delete'])) {
-	echo 'delete';
-	if (!($stmt = $db->prepare("DELETE FROM videoStore WHERE id = ?"))) {
+if($videoName && $videoCat && $len && $notValid == 0 ) {
+	/* Prepared statement, stage 1: prepare */
+	if (!($InsStmt = $db->prepare("INSERT INTO videoStore(name, category, length) VALUES (?, ?, ?)"))) {
 		echo "Prepare failed: (" . $db->errno . ") " . $db->error;
 	}
 
-	if (!$stmt->bind_param("i", $_POST['vid'])) {
+	if (!$InsStmt->bind_param("sss", $videoName, $videoCat, $len)) {
 		echo "Binding parameters failed: (" . $stmt->errno . ") " . $stmt->error;
 	}
 
-	if (!$stmt->execute()) {
+	if (!$InsStmt->execute()) {
 		echo "Execute failed: (" . $stmt->errno . ") " . $stmt->error;
 	}
+	$InsStmt->close();
+}
+
+
+//delete
+if(isset($_POST['delete'])) {
+	if (!($delStmt = $db->prepare("DELETE FROM videoStore WHERE id = ?"))) {
+		echo "Prepare failed: (" . $db->errno . ") " . $db->error;
+	}
+
+	if (!$delStmt->bind_param("i", $_POST['vid'])) {
+		echo "Binding parameters failed: (" . $delStmt->errno . ") " . $delStmt->error;
+	}
+
+	if (!$delStmt->execute()) {
+		echo "Execute failed: (" . $delStmt->errno . ") " . $delStmt->error;
+	}
+	
+	$delStmt->close();
 }
 
 //update
@@ -111,58 +130,126 @@ if(isset($_POST['check'])) {
 	$stmt2->close();
 }
 
+if(isset($_POST['delAll'])) {
+	echo "delete all";
+	if (!($stmt4 = $db->prepare("DELETE FROM videoStore"))) {
+		echo "Prepare failed: (" . $db->errno . ") " . $db->error;
+	}
 
-
-if($videoName && $videoCat && $len && $notValid == 0 ) {
-	/* Prepared statement, stage 1: prepare */
-if (!($stmt = $db->prepare("INSERT INTO videoStore(name, category, length) VALUES (?, ?, ?)"))) {
-    echo "Prepare failed: (" . $db->errno . ") " . $db->error;
+	if (!$stmt4->execute()) {
+		echo "Execute failed: (" . $stmt4->errno . ") " . $stmt4->error;
+	}
+		
+	$stmt4->close();
 }
 
-if (!$stmt->bind_param("sss", $videoName, $videoCat, $len)) {
-    echo "Binding parameters failed: (" . $stmt->errno . ") " . $stmt->error;
-}
 
-if (!$stmt->execute()) {
-    echo "Execute failed: (" . $stmt->errno . ") " . $stmt->error;
-}
 
-}
+echo "<form action=phpAssignment2.php method=post>";
+echo "<select name='sel'>
+<option value='drama'>drama</option>
+<option value='horror'>horror</option>
+<option value='comedy'>comedy</option>
+<option value='romance'>romance</option>
+<option value='all movies'>all movies</option>
+<option value='action'>action</option>
+</select>";
+echo "<input type=submit value='Submit'>";
+echo "</form>";
+
+
+echo "<form action=phpAssignment2.php method=post>";
+echo "<input type=submit name=delAll value='delete all'>";
+echo "</form>";
 
 //display table
-if (!($result = $db->prepare("SELECT id, name, category, length, rented FROM videoStore"))) {
-    echo "Prepare failed: (" . $db->errno . ") " . $db->error;
+
+if(isset($_POST['sel'])) {
+	$up = 1;
+	if($_POST['sel'] == "all movies") {
+		$up = 0;
+	} else {
+		if (!($result = $db->prepare("SELECT id, name, category, length, rented FROM videoStore WHERE category = ?"))) {
+			echo "Prepare failed: (" . $db->errno . ") " . $db->error;
+		}
+
+		if (!$result->bind_param("s", $_POST['sel'])) {
+			echo "Binding parameters failed: (" . $result->errno . ") " . $result->error;
+		}
+
+		if (!$result->execute()) {
+			echo "Execute failed: (" . $result->errno . ") " . $result->error;
+		}
+			$result->bind_result($id, $videoName, $videoCat, $len, $rent);
+
+		echo "<table border=1>
+		<tr>
+		<th>ID</th>
+		<th>Name</th>
+		<th>Category</th>
+		<th>Length</th>
+		<th>Status</th>
+		</tr>";
+		while($result->fetch()) {
+				echo "<form action=phpAssignment2.php method=post>";
+				echo "<tr>";
+				echo "<td>" . "<input type=text name=vid value=" . $id . " </td>";
+				echo "<td>" . "<input type=text name=vName value=" . $videoName . " </td>";
+				echo "<td>" . "<input type=text name=vCat value=" . $videoCat . " </td>";
+				echo "<td>" . "<input type=text name=vLen value=" . $len . " </td>";
+				
+				if($rent == 1) {
+					$stat = "available";
+				} else {$stat = "checked out";}
+				
+				echo "<td>" . "<input type=text name=vStat value=" . $stat . " </td>";
+				echo "<td>" . "<input type=submit name=delete value='delete'" . "</td>";
+				echo "<td>" . "<input type=submit name=check value='check-in/check-out'" . "</td>";
+				echo "</tr>";
+				echo "</form>";
+			}
+			echo "</table>";
+		$result->close();
+	}
 }
 
-$result->execute();
-$result->bind_result($id, $videoName, $videoCat, $len, $rent);
 
-echo "<table border=1>
-<tr>
-<th>ID</th>
-<th>Name</th>
-<th>Category</th>
-<th>Length</th>
-<th>Status</th>
-</tr>";
-while($result->fetch()) {
-	    echo "<form action=phpAssignment2.php method=post>";
-	    echo "<tr>";
-		echo "<td>" . "<input type=text name=vid value=" . $id . " </td>";
-		echo "<td>" . "<input type=text name=vName value=" . $videoName . " </td>";
-		echo "<td>" . "<input type=text name=vCat value=" . $videoCat . " </td>";
-		echo "<td>" . "<input type=text name=vLen value=" . $len . " </td>";
-		
-		if($rent == 1) {
-			$stat = "available";
-		} else {$stat = "checked out";}
-		
-		echo "<td>" . "<input type=text name=vStat value=" . $stat . " </td>";
-		echo "<td>" . "<input type=submit name=delete value='delete'" . "</td>";
-		echo "<td>" . "<input type=submit name=check value='check-in/check-out'" . "</td>";
-		echo "</tr>";
-		echo "</form>";
+if($up == 0) {
+	if (!($result = $db->prepare("SELECT id, name, category, length, rented FROM videoStore"))) {
+		echo "Prepare failed: (" . $db->errno . ") " . $db->error;
 	}
-	echo "</table>";
-	$result->close();
+
+	$result->execute();
+	$result->bind_result($id, $videoName, $videoCat, $len, $rent);
+
+	echo "<table border=1>
+	<tr>
+	<th>ID</th>
+	<th>Name</th>
+	<th>Category</th>
+	<th>Length</th>
+	<th>Status</th>
+	</tr>";
+	while($result->fetch()) {
+			echo "<form action=phpAssignment2.php method=post>";
+			echo "<tr>";
+			echo "<td>" . "<input type=text name=vid value=" . $id . " </td>";
+			echo "<td>" . "<input type=text name=vName value=" . $videoName . " </td>";
+			echo "<td>" . "<input type=text name=vCat value=" . $videoCat . " </td>";
+			echo "<td>" . "<input type=text name=vLen value=" . $len . " </td>";
+			
+			if($rent == 1) {
+				$stat = "available";
+			} else {$stat = "checked out";}
+			
+			echo "<td>" . "<input type=text name=vStat value=" . $stat . " </td>";
+			echo "<td>" . "<input type=submit name=delete value='delete'" . "</td>";
+			echo "<td>" . "<input type=submit name=check value='check-in/check-out'" . "</td>";
+			echo "</tr>";
+			echo "</form>";
+		}
+		echo "</table>";
+		$result->close();
+}
+
 ?>
